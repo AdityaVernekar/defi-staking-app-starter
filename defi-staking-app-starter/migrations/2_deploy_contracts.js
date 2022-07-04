4a20fef809bf6b5f79f68d1c1917c0a194f3ee9e
Aditya Vernekar <adityaditya711@gmail.com>
2022-07-04 16:11:41 +0530

    day 2

--- defi-staking-app-starter/migrations/2_deploy_contracts.js
+++ defi-staking-app-starter/migrations/2_deploy_contracts.js
@@ -2,8 +2,23 @@ const Tether = artifacts.require("Tether");
 const RWD = artifacts.require("RWD");
 const DecentraBank = artifacts.require("DecentraBank");
 
-module.exports = async function(deployer) {
+module.exports = async function(deployer, network, accounts) {
   await deployer.deploy(Tether);
   await deployer.deploy(RWD);
-  await deployer.deploy(DecentraBank);
+
+  // get the deployed Reward Contract
+  const rwd = await RWD.deployed();
+
+  // get the deployed Tether Contract
+  const tether = await Tether.deployed();
+
+  await deployer.deploy(DecentraBank, rwd.address, tether.address);
+  const decentraBank = await DecentraBank.deployed();
+
+  //  tranfer all rwd tokens to the DecentraBank
+  await rwd.transfer(decentraBank.address, "1000000000000000000000000");
+
+  // distribute 100 tether tokens to each investor
+
+  await tether.transfer(accounts[1], "100000000000000000000");
 };
--- defi-staking-app-starter/src/contracts/DecentraBank.sol
+++ defi-staking-app-starter/src/contracts/DecentraBank.sol
@@ -1,10 +1,17 @@
 pragma solidity >=0.5.0 <0.9.0;
 
+import "./Tether.sol";
+import "./RWD.sol";
+
 contract DecentraBank {
     string public name = "DecentraBank";
     address public owner;
+    Tether public tether;
+    RWD public rwd;
 
-    constructor() public {
+    constructor(RWD _rwd, Tether _tether) public {
+        rwd = _rwd;
+        tether = _tether;
         owner = msg.sender;
     }
 }
